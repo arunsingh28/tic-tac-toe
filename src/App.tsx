f72de8baa0c23d1f5e677f3b36ecbaf6b6bd57f6
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-03-09 18:23:22 +0530

    scss adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Layout from './components/layout'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <Layout />
+    )
 }
 
 export default App
---
+++ src/components/layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Layout = () => {
+    return (
+        <div>
+            <h1>Tic tac toe Game</h1>
+            <div className='b-full'>
+                <div className='flex'>
+                    <div>11</div>
+                    <div>12</div>
+                    <div>13</div>
+                </div>
+                <div className='flex'>
+                    <div>21</div>
+                    <div>22</div>
+                    <div>23</div>
+                </div>
+                <div className='flex'>
+                    <div>21</div>
+                    <div>22</div>
+                    <div>23</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Layout
